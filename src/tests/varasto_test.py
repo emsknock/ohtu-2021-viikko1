08876d28e6b05a58cfaef11bd9bcfe437c1c9e99
Emma Koskinen <25252659+emsknock@users.noreply.github.com>
2021-03-17 16:20:32 +0200

    100% coverage

--- src/tests/varasto_test.py
+++ src/tests/varasto_test.py
@@ -38,3 +38,39 @@ class TestVarasto(unittest.TestCase):
 
         # varastossa pitäisi olla tilaa 10 - 8 + 2 eli 4
         self.assertAlmostEqual(self.varasto.paljonko_mahtuu(), 4)
+
+    def test_virheellinen_alkutilavuus_nollataan(self):
+        v = Varasto(-10)
+        self.assertAlmostEqual(v.tilavuus, 0)
+
+    def test_virheellinen_alkusaldo_nollataan(self):
+        v = Varasto(10, -10)
+        self.assertAlmostEqual(v.saldo, 0)
+
+    def test_ylimaarainen_alkusaldo_hukkuu(self):
+        v = Varasto(10, 20)
+        self.assertAlmostEqual(v.saldo, 10)
+
+    def test_negatiivinen_lisays_ei_huomioida(self):
+        saldo = self.varasto.saldo
+        self.varasto.lisaa_varastoon(-1)
+        self.assertAlmostEqual(self.varasto.saldo, saldo)
+
+    def test_liika_lisays_tayttaa_maksimiin(self):
+        self.varasto.lisaa_varastoon(5000)
+        self.assertAlmostEqual(self.varasto.saldo, 10)
+
+    def test_negatiivinen_otto_palauttaa_nolla(self):
+        self.assertAlmostEqual(self.varasto.ota_varastosta(-1), 0)
+
+    def test_yli_saldon_ottaminen_palauttaa_kaiken(self):
+        self.varasto.lisaa_varastoon(5)
+        self.assertAlmostEqual(self.varasto.ota_varastosta(5000), 5)
+
+    def test_yli_saldon_ottaminen_nollaa_saldon(self):
+        self.varasto.lisaa_varastoon(5)
+        self.varasto.ota_varastosta(5000)
+        self.assertAlmostEqual(self.varasto.saldo, 0)
+
+    def test_tostring(self):
+        self.assertEqual(str(self.varasto), "saldo = 0, vielä tilaa 10")
